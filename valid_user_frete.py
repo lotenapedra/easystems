529e4c1d92901d53c74a8b1361edacd1f4f3300e
Eliel Antonin <117757626+lotenapedra@users.noreply.github.com>
2023-07-10 13:28:05 -0300

    Update valid_user_frete.py

--- valid_user_frete.py
+++ valid_user_frete.py
@@ -13,23 +13,28 @@ def verifica_login(usuario, senha):
 
 
 # Tela de login
+import sqlite3
 import streamlit as st
 
+# Função para verificar as credenciais de login
+def verifica_login(usuario, senha):
+    conn = sqlite3.connect('novo.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM usuarios WHERE user = ? AND senha = ?", (usuario, senha))
+    resultado = cursor.fetchone()
+    conn.close()
+    return resultado is not None
+
+# Tela de login
 def tela_login():
     st.title("Acesso Solicitante Frete")
     usuario = st.text_input("Usuário")
     senha = st.text_input("Senha", type="password")
     if st.button("Login"):
-        resultado = verifica_login(usuario, senha)
-        if resultado:
+        if verifica_login(usuario, senha):
             st.success("Login realizado com sucesso!")
             st.markdown("[Clique aqui para acessar o sistema](https://easystems-0ixw0ptprokl.streamlit.app/)")
         else:
             st.error("Credenciais inválidas!")
-
-def verifica_login(usuario, senha):
-    # Implemente a lógica de verificação de login aqui
-    # Retorne True se as credenciais forem válidas, caso contrário, retorne False
-    return True  # Altere conforme sua lógica de verificação
 
 tela_login()
