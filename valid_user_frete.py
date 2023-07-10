c4586cefe0ec6a6bcde05fe09637adfe1084d1c7
Eliel Antonin <117757626+lotenapedra@users.noreply.github.com>
2023-07-10 13:05:07 -0300

    Update valid_user_frete.py

--- valid_user_frete.py
+++ valid_user_frete.py
@@ -14,29 +14,16 @@ def verifica_login(usuario, senha):
 
 # Tela de login
 
-import streamlit as st
-import webbrowser
-
 def tela_login():
-    st.title("Acesso Solicitante Frete")
-    usuario = st.text_input("Usuário")
-    senha = st.text_input("Senha", type="password")
-    if st.button("Login"):
-        resultado = verifica_login(usuario, senha)
-        if resultado:
-            st.success("Login realizado com sucesso!")
-            abrir_url("https://easystems-0ixw0ptprokl.streamlit.app/")
-        else:
-            st.error("Credenciais inválidas!")
-
-def verifica_login(usuario, senha):
-    # Implemente a lógica de verificação de login aqui
-    # Retorne True se as credenciais forem válidas, caso contrário, retorne False
-    return True  # Altere conforme sua lógica de verificação
-
-def abrir_url(url):
-    webbrowser.open_new_tab(url)
+st.title("Acesso Solicitante Frete")
+usuario = st.text_input("Usuário")
+senha = st.text_input("Senha", type="password")
+if st.button("Login"):
+resultado = verifica_login(usuario, senha)
+if resultado:
+st.success("Login realizado com sucesso!")
+st.markdown("[Clique aqui para acessar o sistema](https://easystems-0ixw0ptprokl.streamlit.app/)")
+else:
+st.error("Credenciais inválidas!")
 
 tela_login()
-Neste código, ao clicar no botão "Login" e ter as credenciais corretas, a função abrir_url será chamada para abrir o URL especificado em um navegador externo. Tenha em mente que isso abrirá uma nova janela ou guia no navegador padrão do usuário.
-
